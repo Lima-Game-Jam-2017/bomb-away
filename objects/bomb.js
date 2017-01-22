8d7ab852b4b70af8aac471a02af21317c6f837e5
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2017-01-21 19:23:10 -0500

    add bomb detonation

--- objects/bomb.js
+++ objects/bomb.js
@@ -1,8 +1,15 @@
-Bomb = function(game, x, y) {
+const BombType = {
+    DANGEROUS_BOMB_STRAIGHT_WAVE: 'bomb_img',
+    DANGEROUS_BOMB_DIAGONAL_WAVE: 'bomb_img',
+    SAFE_BOMB: 'bomb_img'
+};
 
-	Phaser.Sprite.call(this, game, x, y, 'bomb_img');
+Bomb = function(game, type, x, y) {
+	Phaser.Sprite.call(this, game, x, y, type);
 
+    this.type = type;
 	this.anchor.setTo(0.5, 0.5);
+    this.inputEnabled = true;
 	game.add.existing(this);
 };
 
@@ -15,3 +22,9 @@ Bomb.prototype.setupBody = function() {
 	this.body.velocity.y = 100 + Math.random() * 100;
 	this.body.bounce.setTo(0.9, 0.9);
 };
+
+Bomb.prototype.detonate = function() {
+    // Show sprites for waves
+    this.kill();
+    this.destroy();
+}
--- states/game.js
+++ states/game.js
@@ -19,9 +19,17 @@ Game.prototype = {
 		this.bombs.physicsBodyType = Phaser.Physics.ARCADE;
 
 		for (var i = 0; i < total; ++i) {
-			var bomb = new Bomb(this, i * 10 + 80, i * 10 + 80);
+			var bomb = new Bomb(this, BombType.DANGEROUS_BOMB_STRAIGHT_WAVE, i * 10 + 80, i * 10 + 80);
 			this.bombs.add(bomb);
 			bomb.setupBody();
+			bomb.events.onInputDown.add(this.onBombPressed, this);
 		}
+	},
+
+	onBombPressed: function(bomb, pointer) {
+		bomb.detonate();
+		this.bombs.remove(bomb);
+		console.log(bomb);
+		console.log(pointer);
 	}
 };
